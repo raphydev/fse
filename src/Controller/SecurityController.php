107f4d637b5ef1f7a310122b216cf264e2a2faf0
raphael Blehoue <raphaelsampan@gmail.com>
2018-04-11 14:18:40 +0000

    creation de la page signin_page

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,18 +2,26 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends Controller
 {
     /**
-     * @Route("/accounts/signin", name="sigin_page")
+     * @Route("/accounts/signin", name="signin_page")
+     * @param Request $request
+     * @param AuthenticationUtils $authenticationUtils
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function login(Request $request, AuthenticationUtils $authenticationUtils)
     {
-        return $this->render('security/index.html.twig', [
-            'controller_name' => 'SecurityController',
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render('security/signin_page.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error
         ]);
     }
 }
